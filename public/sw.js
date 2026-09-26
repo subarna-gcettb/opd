e5bc9e581700fbf51d4a830dd4f3492cdb0d714c
Subarna Adhikari <Subarna.Adhikari@gcettb.ac.in>
2026-09-26 05:34:13 +0530

    fix: purge stale service worker caches on activation

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,7 @@
 const CACHE='chhayabithi-hms-v3';
 const SHELL=['/','/css/style.css','/css/public-bootstrap-fallback.css','/js/main.js','/manifest.json'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',event=>event.waitUntil(self.clients.claim()));
+self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{
   if(event.request.method!=='GET') return;
   const url=new URL(event.request.url);
