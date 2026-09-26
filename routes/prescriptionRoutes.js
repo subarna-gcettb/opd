f64e323d594763d048cf36b4a38b80f5592e2069
Subarna Adhikari <Subarna.Adhikari@gcettb.ac.in>
2026-09-26 06:34:15 +0530

    feat: add hard-copy prescription routes

--- routes/prescriptionRoutes.js
+++ routes/prescriptionRoutes.js
@@ -5,6 +5,7 @@ const { requireAuth } = require('../middleware/auth');
 const { requirePermission } = require('../middleware/roles');
 const { csrfProtection } = require('../middleware/csrf');
 const asyncHandler = require('../utils/asyncHandler');
+const { prescriptionUpload } = require('../utils/prescriptionUpload');
 
 router.use(requireAuth);
 
@@ -12,6 +13,18 @@ router.use(requireAuth);
 router.get('/scan', requirePermission('patient.view'), asyncHandler(prescriptionController.scanLookup));
 
 router.post('/', requirePermission('prescription.create'), csrfProtection, asyncHandler(prescriptionController.create));
+router.post(
+  '/visits/:visitId/hard-copy',
+  requirePermission('prescription.attachment.upload'),
+  csrfProtection,
+  prescriptionUpload.single('prescriptionPhoto'),
+  asyncHandler(prescriptionController.uploadHardCopy)
+);
+router.get(
+  '/attachments/:attachmentId',
+  requirePermission('patient.view'),
+  asyncHandler(prescriptionController.downloadHardCopy)
+);
 router.get('/:id', requirePermission('patient.view', 'prescription.create'), asyncHandler(prescriptionController.view));
 router.get('/:id/print', requirePermission('patient.view', 'prescription.create'), asyncHandler(prescriptionController.print));
 router.post('/:id/amend', requirePermission('prescription.amend'), csrfProtection, asyncHandler(prescriptionController.amend));
